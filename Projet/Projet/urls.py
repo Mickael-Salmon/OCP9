4bec02c1487253881a4847612cf5f5136ec34991
Mickael-Salmon <mickael.salmon@outlook.com>
2023-06-08 22:29:43 +0200

    Running script to create project structure

--- Projet/Projet/urls.py
+++ Projet/Projet/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for OCP9 project.
+URL configuration for Projet project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
