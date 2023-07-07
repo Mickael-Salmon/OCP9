18d1efcba525e9e4fa8e7da2c997bf1960ff86de
mickael.salmon@outlook.com <mickael.salmon@outlook.com>
2023-07-07 11:32:41 +0200

    Created DB and started creating first models

--- Lit_REVIEW/lit_R/models.py
+++ Lit_REVIEW/lit_R/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+#Model to represent an element, book, etc...
+class Ticket(models.Model):
+    pass
+
+#Model Review to represent reviews done on a ticket
