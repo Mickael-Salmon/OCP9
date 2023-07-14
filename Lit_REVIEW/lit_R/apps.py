f3b9d9c2b7b24030f2b4c9d93319630351d09a88
mickael.salmon@outlook.com <mickael.salmon@outlook.com>
2023-07-14 15:08:52 +0200

    removed files

--- Lit_REVIEW/lit_R/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class LitRConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'lit_R'
--- Lit_REVIEW/lit_R/forms.py
+++
@@ -1,3 +0,0 @@
-﻿from crispy_forms.helper import FormHelper
-from crispy_forms.layout import Submit
-from crispy_forms.bootstrap import FormActions
